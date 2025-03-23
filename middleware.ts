6ba4bf0d26b20f885988376bdcb21e2349469f03
DevLaukey <devlaukey@gmail.com>
2025-03-23 11:31:33 +0100

    added middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,16 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({});
+export default clerkMiddleware();
 
+// Configure the middleware to exclude webhook routes
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api/webhooks routes
+     * 2. /_next (Next.js internals)
+     * 3. static files (.css, .js, .ico, etc)
+     */
+    "/((?!api/webhooks|_next|.*\\..*).*)",
+  ],
+};
